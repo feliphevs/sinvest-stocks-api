5fc0110611d3b33a78f6017b4773f975c71d94e7
Feliphe Silva <feliphevs90@outlook.com>
2022-01-12 17:59:52 -0300

    commit inicial

---
+++ src/main/java/com/stocks/sinveststocksapi/SinvestStocksApiApplication.java
@@ -0,0 +1,13 @@
+package com.stocks.sinveststocksapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SinvestStocksApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SinvestStocksApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/stocks/sinveststocksapi/SinvestStocksApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stocks.sinveststocksapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SinvestStocksApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
